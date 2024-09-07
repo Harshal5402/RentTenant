c637718376810f6690ca9392b7eb90e5d794ebdf
Harshal Ingle <harshalingle11092001@gmail.com>
2024-09-07 19:43:33 +0530

    first commit

---
+++ Frontend/Component/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.css" 
+
+const Home = () => {
+  return (
+    <div className='Home'>
+        <button className='SignUp'>Sign Up</button>
+        <button className='SignIn'>Sign In</button>
+    </div>
+  )
+}
+
+export default Home
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Home from '../Component/Home/Home'
+
+function App() {
+
+  return (
+    <>
+    <Home />
+    </>
+  )
+}
+
+export default App
