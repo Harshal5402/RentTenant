82bc81788dabbba0af6d07f43f43f5ff5a4fc279
Harshal Ingle <harshalingle11092001@gmail.com>
2024-09-07 19:49:46 +0530

    Changes

--- Frontend/Component/Home/Home.jsx
+++ Frontend/Component/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./Home.css" 
+import Navbar from '../Navbar/Navbar'
 
 const Home = () => {
   return (
+    <>
+    <Navbar/>
     <div className='Home'>
         <button className='SignUp'>Sign Up</button>
         <button className='SignIn'>Sign In</button>
     </div>
+    </>
   )
 }
 
