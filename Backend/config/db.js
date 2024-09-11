f4779f6e6749014c43c883a434522fe1218b827d
Harshal Ingle <harshalingle11092001@gmail.com>
2024-09-11 17:50:27 +0530

    chane

--- Backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://RentTenant:Rent_Tenant@cluster0.plafw.mongodb.net/RentTenant')
-    .then(() => console.log('Connecting to the DataBase'))
-    .catch(err => console.error('Error Connecting to the DataBase', err))
-}
-
